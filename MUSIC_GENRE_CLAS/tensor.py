f8435acecee143172d67c3e1588ad860ff362c1f
furkan <47noctis@gmail.com>
2024-05-13 21:19:02 +0200

    neural network added

---
+++ MUSIC_GENRE_CLAS/tensor.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+print(tf.__version__)
+
+
